55956bf2cc54d7470957d132313ac8690cf7921f
Mustac <mustac.marijan@gmail.com>
2023-10-15 10:55:44 +0200

    Added confirmation modal to be used programaticaly inside @code{ } .
    Static class ConfirmationModal can call the component ConfirmationModalInstance and get back result, can be called like :
    
    bool actionClicked = await ConfirmationModal.ShowAsync("Test");

---
+++ PizzaPlace.BlazorServer/Shared/Component/ConfirmationModalPackage/ConfirmationModal.cs
@@ -0,0 +1,15 @@
+﻿namespace PizzaPlace.BlazorServer.Shared.Component.ConfirmationModalPackage
+{
+    public static class ConfirmationModal
+    {
+        // Delegate and event to trigger modal
+        internal delegate Task<bool> ShowModalDelegate(string text);
+        internal static event ShowModalDelegate OnShowModal;
+
+        public async static Task<bool> ShowAsync(string text)
+        {
+            // Ensure that OnShowModal is not null before invoking
+            return OnShowModal is null? false: await OnShowModal.Invoke(text);
+        }
+    }
+}
